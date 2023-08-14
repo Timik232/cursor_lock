a00128a3eeb27a59ea6594824fb5234dea82daeb
Timur <100406268+Timik232@users.noreply.github.com>
2023-08-14 13:12:43 +0300

    version 1.0

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 import sys
 import keyboard
 import pyautogui
+from PyQt5.uic.properties import QtCore
 from screeninfo import get_monitors
 import win32api
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWinExtras import QWinTaskbarButton
+from PyQt5 import QtGui, QtWidgets
 import time
 import threading
 import PyQt5.QtGui
 import PyQt5.QtCore
+import ctypes
+myappid = 'Timur.CursorBlocker.1.0'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 screen_size = pyautogui.size()
 xMin = 0
@@ -28,10 +34,8 @@ class CursorBlocker(QWidget):
         self.max_number = len(self.monitors)
         self.number = 0
         self.active_monitor = self.monitors[self.number]
-        self.timer = QTimer(self)
+        self.running = False
         self.initUI()
-        self.timer.timeout.connect(self.block_cursor)
-
 
     def closeEvent(self, event,action=False):
         if not action:
@@ -50,7 +54,8 @@ class CursorBlocker(QWidget):
     def show_application(self):
         self.showNormal()
 
-    def initUI(self):
+    def initUI(self):        
+        icon = 'icon.ico'
         self.setWindowTitle("Cursor Blocker")
         # Создаем кнопки и метки для отображения статуса
         self.start_button = QPushButton("Start", self)
@@ -60,8 +65,12 @@ class CursorBlocker(QWidget):
         self.stop_button.setEnabled(False)
         self.label = QLabel("Cursor unblocked.", self)
         self.label_monitor = QLabel(f"Monitor {self.number + 1}", self)
-        self.tint = QLabel("Press shift + L to toggle block", self)
-        self.tint_change = QLabel("Press shift + ] to change monitor", self)
+        self.tint = QLabel("Press ctrl + L to toggle block", self)
+        self.tint_change = QLabel("Press ctrl + ] or + [ to change monitor", self)
+        self.taskbar_button = QWinTaskbarButton(self)
+        self.taskbar_button.setWindow(self.windowHandle())
+        self.taskbar_button.setOverlayIcon(QtGui.QIcon(icon))
+
 
 
         # Размещаем элементы на форме
@@ -75,12 +84,12 @@ class CursorBlocker(QWidget):
         self.global_listener_thread = threading.Thread(target=self.global_listener)
         self.global_listener_thread.daemon = True
         self.global_listener_thread.start()
-
+        self.setWindowIcon(QIcon(icon))
         self.tray_icon = QSystemTrayIcon(self)
-        self.tray_icon.setIcon(QIcon('icon.ico'))
+        self.tray_icon.setIcon(QIcon(icon))
         self.tray_icon.setVisible(True)
         self.tray_icon.setToolTip('Cursor Blocker')
-        self.setWindowIcon(QIcon('icon.ico'))
+
         # Создаем меню для значка в системном лотке
         menu = QMenu()
         exit_action = QAction('Exit', self)
@@ -90,31 +99,32 @@ class CursorBlocker(QWidget):
         menu.addAction(open_action)
         menu.addAction(exit_action)
         self.tray_icon.setContextMenu(menu)
-        #keyboard.add_hotkey('shift+l', self.toggle())
-
         self.setGeometry(300, 300, 300, 180)
         self.show()
 
     def global_listener(self):
-        keyboard.add_hotkey('shift+l', self.toggle)
-        keyboard.add_hotkey('shift+]', lambda: self.next_monitor(True))
-        keyboard.add_hotkey('shift+[', lambda: self.next_monitor(False))
+        keyboard.add_hotkey('ctrl+l', self.toggle)
+        try:
+            keyboard.add_hotkey('ctrl+]', lambda: self.next_monitor(True))
+            keyboard.add_hotkey('ctrl+[', lambda: self.next_monitor(False))
+        except ValueError:
+            keyboard.add_hotkey('ctrl+ъ', lambda: self.next_monitor(True))
+            keyboard.add_hotkey('ctrl+х', lambda: self.next_monitor(False))
         keyboard.wait()
 
     def start_block(self):
         self.start_button.setEnabled(False)
         self.stop_button.setEnabled(True)
         self.label.setText("Cursor blocked.")
-        self.timer.start(1)
-        # self.block_cursor()
+        self.running = True
+        self.block_cursor()
 
     def stop_block(self):
         self.start_button.setEnabled(True)
         self.stop_button.setEnabled(False)
         self.label.setText("Cursor unblocked.")
-        self.timer.stop()
+        self.running = False
         win32api.ClipCursor((0, 0, 0, 0))
-
 
     def block_cursor(self):
         monitor = self.active_monitor
@@ -125,8 +135,8 @@ class CursorBlocker(QWidget):
         win32api.ClipCursor((left, top, right, bottom))
 
     def toggle(self):
-        if self.timer.isActive():
-            self.timer.stop()
+        if self.running:
+            self.running = False
             self.start_button.setEnabled(True)
             self.stop_button.setEnabled(False)
             self.label.setText("Cursor unblocked.")
@@ -135,8 +145,8 @@ class CursorBlocker(QWidget):
             self.start_button.setEnabled(False)
             self.stop_button.setEnabled(True)
             self.label.setText("Cursor blocked.")
-            self.timer.start(1)
-
+            self.running = True
+            self.block_cursor()
 
     def next_monitor(self, flag):
         if flag:
@@ -149,13 +159,13 @@ class CursorBlocker(QWidget):
             self.number = self.max_number - 1
         self.label_monitor.setText(f"Monitor {self.number + 1}")
         self.active_monitor = self.monitors[self.number]
-        if not self.timer.isActive():}
-            self.timer.stop()
-            self.timer.start(1)
+        if self.running:
+            self.block_cursor()
 
     def keyPressEvent(self, event):
-        if event.nativeScanCode() == 38 and event.modifiers() == Qt.ShiftModifier:
-            self.toggle()
+        pass
+        # if event.nativeScanCode() == 38 and event.modifiers() == Qt.ShiftModifier:
+        #     self.toggle()
 
 
 if __name__ == '__main__':
